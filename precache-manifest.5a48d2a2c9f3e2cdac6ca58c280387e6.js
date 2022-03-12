aedf7f024a4023ed04fc3c0c30844f57dc9047a3
Marina Nakagawa <mari10172001@yahoo.co.jp>
2022-03-12 14:50:18 +0900

    update buildfile

---
+++ precache-manifest.5a48d2a2c9f3e2cdac6ca58c280387e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3b4688ce639a0db03a142f63aa77a3",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "08f3f7a6642d8cdca593",
+    "url": "/calculator/static/css/main.7006b6e3.chunk.css"
+  },
+  {
+    "revision": "77b448ff6ad7c1b5219e",
+    "url": "/calculator/static/js/2.8bce9d5c.chunk.js"
+  },
+  {
+    "revision": "c156324634d5f1effbdced412f76ec11",
+    "url": "/calculator/static/js/2.8bce9d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f3f7a6642d8cdca593",
+    "url": "/calculator/static/js/main.0ff15390.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
