84665401d0558f8e8a23c21fa7e79d350d370ae3
Marina Nakagawa <mari10172001@yahoo.co.jp>
2022-03-12 16:45:03 +0900

    update feature

--- src/App.js
+++ src/App.js
@@ -18,9 +18,17 @@ const toLocaleString = (num) => String(num);
 
 const removeSpaces = (num) => num.toString().replace(/\s/g, "");
 
+const waitSetting = {
+  waitTime: 1000,
+  waitWard: "おまちください",
+}
 
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
 
 const App = () => {
+
   // ステートの宣言
   let [calc, setCalc] = useState({
     // 選択された記号
@@ -30,12 +38,10 @@ const App = () => {
     // 計算された値
     res: 0,
     //　待機中か
-    isWaitting: false
+    isWaitting: false,
+    // メモリー
+    memory: 0,
   });
-
-  const sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
-  }
 
   //////////////////////////////////////////////////////////////////
   // 数字ボタン（0〜9）のいずれかが押されたときだけ起動
@@ -43,9 +49,6 @@ const App = () => {
   const numClickHandler = (e) => {
     e.preventDefault();
     const value = e.target.innerHTML;
-
-    console.log("calc.num",calc.num)
-    console.log("value",value)
     if (removeSpaces(calc.num).length < 16) {
       setCalc({
         ...calc,
@@ -93,7 +96,8 @@ const App = () => {
       ...calc,
       isWaitting: true
     })
-    await sleep(5000);
+    await sleep(waitSetting.waitTime);
+
     if (calc.sign && calc.num) {
       const math = (a, b, sign) =>
         sign === "+"
@@ -120,6 +124,11 @@ const App = () => {
         num: 0,
         isWaitting: false,
       });
+    } else {
+      setCalc({
+        ...calc,
+        isWaitting: false
+      })
     }
   };
 
@@ -137,6 +146,46 @@ const App = () => {
   };
 
   //////////////////////////////////////////////////////////////////
+  // √のときだけ、起動
+  //////////////////////////////////////////////////////////////////
+  const rootClickHandler = () =>{
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
+
+    setCalc({
+      ...calc,
+      num: (Math.sqrt(num)),
+      res: (Math.sqrt(res)),
+    });
+  }
+  
+  //////////////////////////////////////////////////////////////////
+  // ▶のときだけ、一桁目を消す
+  //////////////////////////////////////////////////////////////////
+  const deleteOneDigitClickHandler = () =>{
+    let num = calc.num ? parseFloat(String(calc.num).slice( 0, -1)) : 0;
+    setCalc({
+      ...calc,
+      num: num ,
+    });
+  }
+
+  //////////////////////////////////////////////////////////////////
+  // 00のときだけ、起動
+  //////////////////////////////////////////////////////////////////
+  const doubleClickHandler = () => {
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
+
+    setCalc({
+      ...calc,
+      num: num*100,
+      res: res*100,
+      sign: "",
+    });
+  }
+
+  //////////////////////////////////////////////////////////////////
   // percentClickHandler関数は、入力された値（num）または計算された値（res）があるかどうかをチェックし、
   // 組み込みのMath.pow関数（底値から指数への累乗を返す）を使ってパーセンテージを計算する関数です。
   //////////////////////////////////////////////////////////////////  
@@ -152,22 +201,86 @@ const App = () => {
   };
 
   //////////////////////////////////////////////////////////////////
-  // resetClickHandler関数は、calcのすべての初期値をデフォルトにし、
+  // allClearClickHandler関数は、calcのすべての初期値をデフォルトにし、
   // 電卓アプリが最初にレンダリングされたときのcalcの状態を返します。
   ////////////////////////////////////////////////////////////////// 
-  const resetClickHandler = () => {
+  const allClearClickHandler = () => {
     setCalc({
       ...calc,
       sign: "",
       num: 0,
       res: 0,
-      isWaitting: false
+      isWaitting: false,
+      memory: 0,
+    });
+  };
+
+  //////////////////////////////////////////////////////////////////
+  // Cはメモリ以外のcalcのすべての初期値をデフォルトにする
+  ////////////////////////////////////////////////////////////////// 
+  const clearClickHandler = () => {
+    setCalc({
+      ...calc,
+      num: 0,
+      res: 0,
+    });
+  };
+
+  //////////////////////////////////////////////////////////////////
+  // MR（メモリーリコール）: 直前のメモリー計算の結果「メモリー値」を呼び出すキー
+  ////////////////////////////////////////////////////////////////// 
+  const memoryRecallClickHandler = () => {
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+    let memory = calc.memory ? parseFloat(removeSpaces(calc.memory)) : 0;
+    setCalc({
+      ...calc,
+      num: memory
+    });
+  };
+
+  //////////////////////////////////////////////////////////////////
+  // MC（メモリークリア）: 現在の「メモリー値」をクリアするキー
+  ////////////////////////////////////////////////////////////////// 
+  const memoryClearClickHandler = () => {
+    setCalc({
+      ...calc,
+      num: 0,
+      memory: 0,
+    });
+  };
+
+  //////////////////////////////////////////////////////////////////
+  // M+（メモリープラス）: 直前の数値または計算結果を「メモリー値」に足す時に押すキー
+  ////////////////////////////////////////////////////////////////// 
+  const memoryPulusClickHandler = () => {
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+    let memory = calc.memory ? parseFloat(removeSpaces(calc.memory)) : 0;
+    
+    setCalc({
+      ...calc,
+      // 入力された値
+      num: 0,
+      memory: memory + num,
+    });
+  };
+
+  //////////////////////////////////////////////////////////////////
+  // M-（メモリーマイナス）: 直前の数値または計算結果を「メモリー値」に足す時に押すキー
+  ////////////////////////////////////////////////////////////////// 
+  const memoryMinusClickHandler = () => {
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+    let memory = calc.memory ? parseFloat(removeSpaces(calc.memory)) : 0;
+
+    setCalc({
+      ...calc,
+      num: 0,
+      memory: memory - num,
     });
   };
 
   return (
     <Wrapper>
-      <Screen value={ calc.isWaitting ? "おまちください" : calc.num ? calc.num : calc.res} />
+      <Screen value={ calc.isWaitting ? waitSetting.waitWard : calc.num ? calc.num : calc.res} />
       <ButtonBox>
         {btnValues.flat().map((btn, i) => {
           return (
@@ -176,10 +289,26 @@ const App = () => {
               className={btn === "=" ? "equals" : ""}
               value={btn}
               onClick={
-                btn === "C"
-                  ? resetClickHandler
+                btn === "AC"
+                  ? allClearClickHandler
+                  : btn === "C"
+                  ? clearClickHandler
                   : btn === "+/-"
                   ? invertClickHandler
+                  : btn === "√"
+                  ? rootClickHandler
+                  : btn === "00"
+                  ? doubleClickHandler
+                  : btn === "▶"
+                  ? deleteOneDigitClickHandler
+                  : btn === "MR"
+                  ? memoryRecallClickHandler
+                  : btn === "MC"
+                  ? memoryClearClickHandler
+                  : btn === "M+"
+                  ? memoryPulusClickHandler
+                  : btn === "M-"
+                  ? memoryMinusClickHandler
                   : btn === "%"
                   ? percentClickHandler
                   : btn === "="
